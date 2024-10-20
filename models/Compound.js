b28ec82000c2dbfcc8a3d3856190af0ee23a9392
Esteban <122425146+juanespaez@users.noreply.github.com>
2024-10-19 21:31:46 -0500

    Create Compound.js

---
+++ models/Compound.js
@@ -0,0 +1,8 @@
+class Compound {
+    constructor(name, formula, molarMass, state) {
+      this.name = name;
+      this.formula = formula;
+      this.molarMass = molarMass;
+      this.state = state;
+    }
+  }
