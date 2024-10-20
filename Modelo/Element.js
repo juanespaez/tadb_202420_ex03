56e876710fb01bf11200a2dd21d4da1d130ad385
Esteban <122425146+juanespaez@users.noreply.github.com>
2024-10-19 21:17:29 -0500

    Update and rename archivo3.js to Element.js
    We rename the file and added the code related to its new name

---
+++ Modelo/Element.js
@@ -0,0 +1,8 @@
+class Element {
+    constructor(name, symbol, atomicNumber, electronicConfig) {
+      this.name = name;
+      this.symbol = symbol;
+      this.atomicNumber = atomicNumber;
+      this.electronicConfig = electronicConfig;
+    }
+  }
