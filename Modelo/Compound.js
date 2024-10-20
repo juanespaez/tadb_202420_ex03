7a35c8881a56f69142ca8f63d08bab9ea7ebea2f
Esteban <122425146+juanespaez@users.noreply.github.com>
2024-10-19 21:32:04 -0500

    Delete Modelo directory

--- Modelo/Compound.js
+++
@@ -1,8 +0,0 @@
-class Compound {
-    constructor(name, formula, molarMass, state) {
-      this.name = name;
-      this.formula = formula;
-      this.molarMass = molarMass;
-      this.state = state;
-    }
-  }
