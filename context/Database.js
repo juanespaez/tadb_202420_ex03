da0b48ef6bccda17cf7a83d43835d8804b422eaf
Juan José  Rios <JuanJRiosP9@gmail.com>
2024-10-23 08:07:37 -0500

    Update Database.js
    Contexto para permitir la conexión a la base de datos

--- context/Database.js
+++ context/Database.js
@@ -1,1 +1,23 @@
 const = "hola profe";
+
+const { Pool } = require('pg');
+require('dotenv').config();
+ 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE
+});
+ 
+ 
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Database connected successfully');
+  }
+});
+ 
+module.exports = pool;
