2321dd34a875a43af95538d06400d725647a7634
Esteban <122425146+juanespaez@users.noreply.github.com>
2024-10-24 18:06:24 -0500

    Update elementService.js

--- service/elementService.js
+++ service/elementService.js
@@ -1,1 +1,33 @@
-const = "hola profe";
+const Element = require('../models/Element');
+const elementRepository = require('../repository/elementRepository');
+
+class ElementService {
+  async getAllElements() {
+    return await elementRepository.findAll();
+  }
+
+  async getElementBySymbol(symbol) {
+    const element = await elementRepository.findById(symbol);
+    if (!element) {
+      throw new Error('Element not found');
+    }
+    return element;
+  }
+
+  async createElement(elementData) {
+    const element = new Element(elementData.namee, elementData.symbol, elementData.atomicNumber, elementData.electronicConfig);
+    return await elementRepository.create(element);
+  }
+
+  async updateElement(id, elementData) {
+    const element = new Element(elementData.namee, elementData.symbol, elementData.atomicNumber, elementData.electronicConfig);
+    await elementRepository.update(id, element);
+  }
+
+  async deleteElement(id) {
+    const delete_element = await elementRepository.delete(id);
+    return delete_element;
+  }
+}
+
+module.exports = new ElementService();
